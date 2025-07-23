be29c47044d5eb192ec1d0ef3d7a12543db4d1e1
Matt Metcalf <mrm9084@gmail.com>
2025-07-23 11:27:38 -0700

    cleaned up history

--- Backend/azure_open_ai_service.py
+++ Backend/azure_open_ai_service.py
@@ -29,55 +29,50 @@ class AzureOpenAIService:
         Get chat completion from Azure OpenAI service.
         """
 
-        agent  = self.project_client.agents.get_agent(
-            agent_id=self.assistant_id
-        )
+        agent = self.project_client.agents.get_agent(agent_id=self.assistant_id)
 
         # Create a thread for communication
         thread = self.project_client.agents.threads.create()
-        
+
         # Add conversation history
         for message in request.history:
             self.project_client.agents.messages.create(
-                thread_id=thread.id,
-                content=message.content,
-                role= message.role
+                thread_id=thread.id, content=message.content, role=message.role
             )
 
         # Add current user message
         self.project_client.agents.messages.create(
-            thread_id=thread.id,
-            content=request.message,
-            role="user"
+            thread_id=thread.id, content=request.message, role="user"
         )
 
         # Create and process an agent run
-        self.project_client.agents.runs.create_and_process(thread_id=thread.id, agent_id=agent.id)
+        self.project_client.agents.runs.create_and_process(
+            thread_id=thread.id, agent_id=agent.id
+        )
 
-
-        response =  self.project_client.agents.messages.list(thread_id=thread.id)
+        response = self.project_client.agents.messages.list(thread_id=thread.id)
 
         response_content = None
 
         for msg in response:
             if msg.role == "assistant":
-                response_content =  msg.content[0].text.value
+                response_content = msg.content[0].text.value
                 break
 
         # Update history
         history = request.history.copy()
-        history.append(
-            ChatbotMessage(
-                role="user",
-                content=request.message,
-                timestamp=datetime.now(tz=timezone.utc),
-            )
-        )
-        history.append(
-            ChatbotMessage(
-                role="assistant",
-                content=response_content,
-                timestamp=datetime.now(tz=timezone.utc),
-            )
+        history.extend(
+            [
+                ChatbotMessage(
+                    role="user",
+                    content=request.message,
+                    timestamp=datetime.now(tz=timezone.utc),
+                ),
+                ChatbotMessage(
+                    role="assistant",
+                    content=response_content,
+                    timestamp=datetime.now(tz=timezone.utc),
+                ),
+            ]
         )
         return ChatResponse(message=response_content, history=history)
